22ab8af5955abdf6dfdd546db54a90dc1db6b265
Graham Abbott <graham.abbott@gmail.com>
2017-01-08 11:07:23 -0800

    prep for pypi

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.1',
+    version='1.0.2',
 
     description='RFC 4648 Extended Hex Alphabet',
     long_description=long_description,
@@ -32,5 +32,5 @@ setup(
     license='MIT',
 
     py_modules=['base32hex'],
-    download_url="https://github.com/graham/base32hex/tarball/0.2",
+    download_url="https://github.com/graham/base32hex/tarball/0.3",
 )
